06b9caf9ac0677b4690732a6c2f953dbd27a50ec
TanNN <tannnph08772@fpt.edu,vn>
2020-11-14 03:08:23 +0700

    fix_giao_dien

--- resources/views/classes/danh_sach_lop.blade.php
+++ resources/views/classes/danh_sach_lop.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="card shadow mb-4">
     <div class="card-header py-3 d-flex justify-content-between align-items-center">
-        <h6 class="m-0 font-weight-bold text-primary">Danh sách lớp<</h6>
+        <h3 class="m-0 font-weight-bold text-primary">Danh sách lớp</h3>
         <a class="btn btn-success" href="{{ route('classes.create') }}">Tạo Lớp</a>
     </div>
     <div class="card-body">
